78de14cf1ebce739a6fba952ab301696d503e106
Jefferson <jeffersonaraujo491@gmail.com>
2024-01-11 22:51:15 -0500

    base commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center">
+  My dotfiles
+</h1>
---
+++ i3/scripts/autostart.sh
@@ -0,0 +1,12 @@
+# -----------------
+# autostart commands:
+# -----------------
+
+# wallpaper
+nitrogen --set-auto /home/jeff/Pictures/skull.png --head=0 && nitrogen --set-auto /home/jeff/Pictures/skull.png --head=1 --set-color=#1d2021 
+
+# polybar
+$HOME/.config/polybar/launch.sh
+
+# keyboard layout
+setxkbmap -layout latam,latam
